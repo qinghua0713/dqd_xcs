ebd7ec2dadca30e302c599395280a558c1b6716c
deqidao <qingua0713@163.com>
2019-12-23 09:44:45 +0800

    12/22

--- deqidao_xcs/pages/details/details.js
+++ deqidao_xcs/pages/details/details.js
@@ -8,15 +8,41 @@ Page({
       "/assets/image/tunnel_banner2.png",
       "/assets/image/tunnel_banner.png",
     ],//轮播图数据列表
-    current: 0//轮播图当前位置
+    current: 0,//轮播图当前位置
+    //order:true,
+ 
   },
   //点击显示全部
-  viewArr() {
-    this.setData({
-      isShow: !this.data.isShow
-    })
-    console.log(this.data.isShow)
+  // viewArr() {
+  //   this.setData({
+  //     isShow: !this.data.isShow
+  //   })
+  //   console.log(this.data.isShow)
+  // },
+  //点击立即预定按钮事件
+  promptlyOrder(){
+     this.setData({
+       order:true
+     })
   },
+
+  //点击X隐藏遮罩
+  closeShop(){
+    this.setData({
+      order:false
+    })
+  },
+  //防止遮罩穿透
+  // doNotMove(){
+  //   console.log(11)
+  //   return
+  // },
+  //点击显示收货地址模块
+  addAddress(){
+  wx.navigateTo({ url: '/pages/address/address' });
+  },
+  //点击编辑显示地址编辑模块
+ 
   //轮播图改变触发
   swiperChange(e) {
     var that = this;
@@ -24,5 +50,8 @@ Page({
       current: e.detail.current,
     })
   },
+  onLoad(){
+  
+  }
 
 })

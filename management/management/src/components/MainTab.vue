7b89a1e525d463b85c5bec788179e1d7de870b65
deqidao <qingua0713@163.com>
2019-12-30 13:01:17 +0800

    12/30

---
+++ management/management/src/components/MainTab.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="leftmenu">
+    <ul>
+      <li
+        :class="$store.state.num == index ? 'men-item  bg_bor':'men-item '"
+        v-for="(item,index) in tabList"
+        :key="index"
+        @click="currentIndex(index)"
+      >{{item}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainTab",
+  data() {
+    return {
+      tabList: ["艺术品基本详情", "购买设置", "ACS服务合同"],
+      current: 0,
+      currentIdx:0
+    };
+  },
+  methods: {
+    currentIndex(index) {
+      this.currentIdx = (this.$store.state.num = index);
+      localStorage.setItem("num", this.currentIdx);
+    }
+  },
+  destroyed() {
+     this.currentIdx = 0
+     console.log(this.currentIdx)
+  },
+};
+</script>
+
+<style scoped>
+.leftmenu {
+  width: 12%;
+  color: #aaa;
+  text-align: right;
+  float: left;
+}
+.men-item {
+  height: 30px;
+  padding-right: 10px;
+  font-size: 12px;
+  line-height: 30px;
+  margin-bottom: 10px;
+  position: relative;
+}
+.bg_bor {
+  width: 101%;
+  border: 1px solid #333;
+  border-right: none;
+  background: #fff;
+  color: #333;
+}
+</style>
---
+++ management/management/src/components/leftMenu.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="left-menu">
+    <div class="mian-menu">
+      <ul>
+        <li
+          :class="current == index ?'mian-menu-list current_red':'mian-menu-list'"
+          v-for="(item,index) in menuList"
+          :key="index"
+        >{{item}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LeftMenu",
+  data() {
+    return {
+      menuList: ["Banner管理", "搜索管理", "咨询管理", "艺术品档案"],
+      current: 0 //给当前菜单添加样式
+    };
+  },
+  mounted() {
+    this.current = this.menuList.length - 1;
+  }
+};
+</script>
+
+<style scoped>
+</style>
